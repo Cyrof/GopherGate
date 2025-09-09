a2e7b7fbcdb424b992627f8be3b0f2a82cfc0523
keith <keithneo00@gmail.com>
2025-09-09 21:01:43 +0800

    refactor(cmd/, pkg/cobraCLI/): Reformatted the golang files

--- gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
+++ gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "github.com/Cyrof/GopherGate/gophergate-wg-agent/pkg/cobraCLI"
+	"github.com/Cyrof/GopherGate/gophergate-wg-agent/pkg/cobraCLI"
 )
 
 func main() {
-  cobraCLI.Execute()
+	cobraCLI.Execute()
 }
--- gophergate-wg-agent/pkg/cobraCLI/root.go
+++ gophergate-wg-agent/pkg/cobraCLI/root.go
@@ -1,30 +1,30 @@
 package cobraCLI
 
 import (
-  "os"
+	"os"
 
-  "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var (
-  rootCmd = &cobra.Command{
-      Use: "gophergate-wg-agent",
-      Short: "A ClI agent for managing WireGuard with Cobra and gRPC.",
-      Long: `The gophergate-wg-agent provides a simple Cobra-based CLI to manage
+	rootCmd = &cobra.Command{
+		Use:   "gophergate-wg-agent",
+		Short: "A ClI agent for managing WireGuard with Cobra and gRPC.",
+		Long: `The gophergate-wg-agent provides a simple Cobra-based CLI to manage
 WireGuard interfaces and peers. It also exposes a gRPC server to allow
 external tools, such as the gophergate-ui, to interact with the WireGuard
 service for automation and integration.`,
-      PersistentPreRun: func(cmd *cobra.Command, args []string) {
-          // skip command if not runnable
-          if !cmd.Runnable() {
-            return
-          }
-        },
-    }
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			// skip command if not runnable
+			if !cmd.Runnable() {
+				return
+			}
+		},
+	}
 )
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
