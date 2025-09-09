c35924bb2ebc3cda1f34c74618ecedeefcfd8681
Cyrof <keithneo00@gmail.com>
2025-09-09 15:02:30 +0800

    feat(gophergate-wg-agent/): Added the gophergate backend service folder to start building the application
    feat(gophergate-wg-agent/cmd, gophergate-wg-agent/pkg/cobraCLI):
    Configured the skeleton of Cobra cli that will be used in this
    application

---
+++ gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/Cyrof/GopherGate/gophergate-wg-agent/pkg/cobraCLI"
+)
+
+func main() {
+  cobraCLI.Execute()
+}
---
+++ gophergate-wg-agent/pkg/cobraCLI/root.go
@@ -0,0 +1,27 @@
+package cobraCLI
+
+import (
+  "os"
+
+  "github.com/spf13/cobra"
+)
+
+var (
+  rootCmd = &cobra.Command{
+      Use: "gophergate-wg-agent",
+      Short: "A ClI agent for managing WireGuard with Cobra and gRPC.",
+      Long: "The gophergate-wg-agent provides a simple Cobra-based CLI to manage WireGuard interfaces and peers. It also exposes a gRPC server to allow external tools, such as the gophergate-ui, to interact with the WireGuard service for automation and integration.",
+      PersistentPreRun: func(cmd *cobra.Command, args []string) {
+          // skip command if not runnable
+          if !cmd.Runnable() {
+            return
+          }
+        },
+    }
+)
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    os.Exit(1)
+  }
+}
