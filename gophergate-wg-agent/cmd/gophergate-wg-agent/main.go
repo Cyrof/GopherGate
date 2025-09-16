b1a8fa9c5fcb3be801f173c4bae893621bf75021
Keith <73649950+Cyrof@users.noreply.github.com>
2025-09-16 08:45:48 +0000

    refator(gophergate-wg-agent/cmd/): Initialised the default path for gophergate-wg-agent

--- gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
+++ gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"log"
+
+	// "github.com/Cyrof/GopherGate/gophergate-core/logx"
+	"github.com/Cyrof/GopherGate/gophergate-core/paths"
+
 	"github.com/Cyrof/GopherGate/gophergate-wg-agent/pkg/cobraCLI"
 )
 
 func main() {
+	p := paths.ForApp("gophergate-wg-agent")
+	if err := p.Ensure(); err != nil {
+		log.Fatalf("failed to ensure paths: %v", err)
+	}
+
+	// logger := logx.Init(logx.Default("gophergate-wg-agent"))
+
 	cobraCLI.Execute()
 }
