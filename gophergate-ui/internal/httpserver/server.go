a8eefeb470893f1cc9bce108dc1fc5e445d5e57a
Shi Min <k.shimin20@gmail.com>
2025-09-20 23:19:45 +0800

    feat(httpserver/, templates/): Customized gophergate logger to use Gin default logger for HTTP requests. Also created a home template to test if the server is working.

--- gophergate-ui/internal/httpserver/server.go
+++ gophergate-ui/internal/httpserver/server.go
@@ -1,3 +1,27 @@
 package httpserver
 
 // this file should hold the initialisation function for the httpserver using gin as the framework
+
+import (
+	"net/http"
+
+	"github.com/Cyrof/GopherGate/gophergate-ui/internal/config"
+	"github.com/Cyrof/GopherGate/gophergate-ui/internal/handlers"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func Run(cfg *config.Config, log *zap.SugaredLogger) error {
+	r := gin.New()
+	r.Use(ZapLogger(log), ZapRecovery(log))
+
+	r.Static("/static", "web/static")
+	r.LoadHTMLGlob("web/templates/*.tmpl")
+
+	r.GET("/", handlers.Home())
+	r.GET("/api/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"ok": true})
+	})
+
+	return r.Run(cfg.HTTPAddr)
+}
