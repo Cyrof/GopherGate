a9d619a4e1816883b9bf3e374e33ef6c35f3b794
Keith <73649950+Cyrof@users.noreply.github.com>
2025-09-29 05:33:10 +0000

    refactor(cobraCLI/edit): Updated the existing edit placeholder to use the wireguard functions created in the previous commit

--- gophergate-wg-agent/pkg/cobraCLI/edit.go
+++ gophergate-wg-agent/pkg/cobraCLI/edit.go
@@ -1,14 +1,25 @@
 package cobraCLI
 
 import (
-	"errors"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
 
+	"github.com/Cyrof/GopherGate/gophergate-wg-agent/internal/wgsvc"
 	"github.com/spf13/cobra"
 )
 
 var (
-	newIP   string
-	newName string
+	upIface         string
+	upPubKey        string
+	upSetAllowed    []string
+	upAppendAllowed []string
+	upEndpoint      string
+	upKeepalive     int
+	upKeepaliveSet  bool
+	upAsJSON        bool
 )
 
 var editCmd = &cobra.Command{
@@ -30,29 +41,64 @@ yet implemented`,
 	gophergate-wg-agent edit --id 456 --name carol --ip 10.0.0.99
 	`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if name == "" && peerID == "" {
-			return errors.New("you must specify either --name or --id to edit a peer")
+		if upPubKey == "" {
+			return fmt.Errorf("--pubkey is required")
 		}
-		if name != "" && peerID != "" {
-			return errors.New("please specify only one of --name or --id, not both")
+		if len(upSetAllowed) > 0 && len(upAppendAllowed) > 0 {
+			return fmt.Errorf("provide either --set-allowed or --append-allowed, not both")
 		}
 
-		// make sure name or ip is specified
-		if newName == "" && newIP == "" {
-			return errors.New("you must specify at least one field to update (--name or --ip)")
-		}
+		upKeepaliveSet = cmd.Flags().Changed("keepalive")
 		return nil
 	},
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println("edit command is not yet implemented")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var kaPtr *int
+		if upKeepaliveSet {
+			ka := upKeepalive
+			kaPtr = &ka
+		}
+
+		req := wgsvc.UpdatePeerRequest{
+			Iface:              upIface,
+			PublicKey:          upPubKey,
+			SetAllowedCIDRs:    upSetAllowed,
+			AppendAllowedCIDRs: upAppendAllowed,
+			Endpoint:           upEndpoint,
+			KeepaliveSeconds:   kaPtr,
+		}
+
+		ctx, cancel := context.WithTimeout(cmd.Context(), 5*time.Second)
+		defer cancel()
+
+		resp, err := wgsvc.UpdatePeer(ctx, req)
+		if err != nil {
+			if strings.Contains(strings.ToLower(err.Error()), "operation not permitted") {
+				Log.Errorw("permission error: need CAP_NET_ADMIN (sudo or setup cap_net_admin=ep)")
+			} else {
+				Log.Errorw("update peer failed", "iface", upIface, "err", err)
+			}
+			return err
+		}
+
+		if upAsJSON {
+			enc := json.NewEncoder(cmd.OutOrStdout())
+			enc.SetIndent("", " ")
+			return enc.Encode(resp)
+		}
+
+		Log.Infow("peer updated", "iface", resp.Iface, "pubKey", resp.PublicKey, "change.allowedIPs", resp.Changed.AllowedIPs, "change.endpoint", resp.Changed.Endpoint, "change.keepalive", resp.Changed.Keepalive)
+		return nil
 	},
 }
 
 func init() {
-	editCmd.Flags().StringVar(&name, "name", "", "Name of the peer to edit")
-	editCmd.Flags().StringVar(&peerID, "id", "", "Unique ID of the peer to edit")
-	editCmd.Flags().StringVar(&newName, "new-name", "", "New name for the peer")
-	editCmd.Flags().StringVar(&newIP, "ip", "", "New IP address for the peer")
+	editCmd.Flags().StringVarP(&upIface, "iface", "i", "wg0", "Wireguard interface name")
+	editCmd.Flags().StringVarP(&upPubKey, "pubkey", "p", "", "Peer public key (base64, required)")
+	editCmd.Flags().StringSliceVar(&upSetAllowed, "set-allowed", nil, "Replace AllowedIPs with these CIDRs (repeatable)")
+	editCmd.Flags().StringSliceVar(&upAppendAllowed, "append-allowed", nil, "Append these CIDRs to AllowedIPs (repeatable)")
+	editCmd.Flags().StringVarP(&upEndpoint, "endpoint", "e", "", "Set peer endpoint (host:port)")
+	editCmd.Flags().IntVarP(&upKeepalive, "keepalive", "k", 0, "Set keepalive second (0 disables). If ommited, unchanged.")
+	editCmd.Flags().BoolVarP(&upAsJSON, "json", "j", false, "Output JSON response")
 
 	rootCmd.AddCommand(editCmd)
 }
