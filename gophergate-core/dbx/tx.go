c1695019c7cc16d9f421063192e3b7d1385120b8
keith <keithneo00@gmail.com>
2025-10-10 15:08:29 +0800

    feat!(gophergate-core/dbx): Added a migrate feature to dbx. This feature allows gophergate-core to parse sql files in a standardised folder structure. For more information, the documentation will be written in the README file

---
+++ gophergate-core/dbx/tx.go
@@ -0,0 +1,31 @@
+package dbx
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func WithTx(ctx context.Context, pool *pgxpool.Pool, fn func(ctx context.Context, tx pgxTx) error) error {
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = tx.Rollback(ctx) }()
+	if err := fn(ctx, tx); err != nil {
+		return err
+	}
+	return tx.Commit(ctx)
+}
+
+type pgxTx interface {
+	Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Rollback(ctx context.Context) error
+	Commit(ctx context.Context) error
+}
+
+var _ pgxTx = (pgx.Tx)(nil)
