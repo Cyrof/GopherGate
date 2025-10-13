ac341faf909634b0737338d8947716af4d29299c
Cyrof <keithneo00@gmail.com>
2025-10-13 15:01:56 +0800

    feat(schema/): Added the base folder for sql files
    refactor(main): Updated the main.go to use the newly updated core
    modules
    
    chore: Updated the dependencies

--- gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
+++ gophergate-wg-agent/cmd/gophergate-wg-agent/main.go
@@ -3,25 +3,49 @@ package main
 import (
 	stdlog "log"
 
+	"context"
+	"time"
+
+	"github.com/Cyrof/GopherGate/gophergate-core/dbx"
+	"github.com/Cyrof/GopherGate/gophergate-core/envx"
 	"github.com/Cyrof/GopherGate/gophergate-core/logx"
 	"github.com/Cyrof/GopherGate/gophergate-core/paths"
 
+	"github.com/Cyrof/GopherGate/gophergate-wg-agent/internal/schema/migration"
 	"github.com/Cyrof/GopherGate/gophergate-wg-agent/pkg/cobraCLI"
 )
 
 var Version = "0.1.0"
 
 func main() {
-	p := paths.ForApp("gophergate-wg-agent")
+	// load .env if present
+	envx.LoadDotenvIfPresent()
+
+	p := paths.ForApp(paths.AppAgent)
 	if err := p.Ensure(); err != nil {
 		stdlog.Fatalf("failed to ensure paths: %v", err)
 	}
 
 	logger, flush := logx.Init(logx.Default(paths.AppAgent))
-
 	defer flush()
 
 	logger.Infow("agent started", "version", Version)
+
+	// initialise database
+	cfg := dbx.Default(paths.AppAgent)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	pool, closePool, err := dbx.Open(ctx, cfg)
+	if err != nil {
+		logger.Fatalw("failed to connect database", "err", err)
+	}
+	defer closePool()
+
+	mcfg := &dbx.MigrateConfig{AdvisoryLockKey: 4242}
+	if err := dbx.MigrateFS(ctx, pool, migration.FS, ".", mcfg); err != nil {
+		logger.Fatalw("migrate failed", "err", err)
+	}
 
 	cobraCLI.SetLogger(logger)
 
--- gophergate-wg-agent/go.mod
+++ gophergate-wg-agent/go.mod
@@ -5,7 +5,7 @@ go 1.24.0
 toolchain go1.24.5
 
 require (
-	github.com/Cyrof/GopherGate/gophergate-core v0.3.3
+	github.com/Cyrof/GopherGate/gophergate-core v0.3.4
 	github.com/spf13/cobra v1.10.1
 	go.uber.org/zap v1.27.0
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20241231184526-a9ab2273dd10
@@ -14,6 +14,10 @@ require (
 require (
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.6 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mdlayher/genetlink v1.3.2 // indirect
 	github.com/mdlayher/netlink v1.8.0 // indirect
@@ -24,6 +28,7 @@ require (
 	golang.org/x/net v0.46.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 	golang.zx2c4.com/wireguard v0.0.0-20250521234502-f333402bd9cb // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
