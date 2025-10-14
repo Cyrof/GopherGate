8d8964e79684d3b19c7577772f6fe3e0d5404965
Cyrof <keithneo00@gmail.com>
2025-10-14 09:34:35 +0800

    refactor(schema/migrations/*.sql): Done up the first phase of the sql schema for postgres

--- gophergate-wg-agent/internal/schema/migration/0001_create_peers.down.sql
+++ gophergate-wg-agent/internal/schema/migration/0001_create_peers.down.sql
@@ -1,13 +1,24 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create extension if not exists "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS peers (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    name text NOT NULL,
-    public_key text NOT NULL,
-    ip_address inet NOT NULL,
-    allowed_ips inet[] DEFAULT '{}',
-    endpoint text,
-    persistent_keepalive smallint,
-    last_handshake timestamptz,
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-    updated_at timestamptz NOT NULL DEFAULT NOW())
+create table if not exists peers(
+	id uuid primary key default uuid_generate_v4(),
+	name text not null,
+	public_key text not null,
+	ip_address inet not null,
+	allowed_ips inet[] default '{}',
+	endpoint text,
+	persistent_keepalive smallint,
+	last_handshake timestamptz,
+	created_at timestamptz not null default now(),
+	updated_at timestamptz not null default now()
+);
+
+-- add uniques for wg agent
+alter table peers
+	add constraint uq_peers_public_key unique (public_key);
+
+alter table peers
+	add constraint uq_peers_ip_address unique (ip_address);
+
+create index if not exists idx_peers_name on peers(name);
+
